c06d8d1b3d986e928c0beb1c8b3b19fbaaf96e16
lynneeai <lynneeai@hotmail.com>
2015-05-11 12:11:49 -0400

    Added test suite documentation; updated UnitTest1.cs

--- ConsoleApplication1/UnitTest1.cs
+++ ConsoleApplication1/UnitTest1.cs
@@ -2,15 +2,16 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace ConsoleApplication1
 {
-    [TestClass]
+    //[TestClass]
     public class UnitTest1
     {
         static Uri testServer = ServerUris.getLatest();
 
-        [TestMethod]
+        //[TestMethod]
         public async Task ICmdTest()
         {
             //Valid
@@ -37,13 +38,14 @@ namespace ConsoleApplication1
             }
         }
 
-        [TestMethod]
+        //[TestMethod]
         public async Task DeviceBackupTest()
         {
 
+
         }
 
-        [TestMethod]
+        //[TestMethod]
         public async Task DeviceScanTest()
         {
 
@@ -65,15 +67,61 @@ namespace ConsoleApplication1
             }            
         }
 
-        [TestMethod]
+        //[TestMethod]
         public async Task DeviceSettingTest()
         {
+			//Valid
+			DeviceSetting dSetting1 = new DeviceSetting(testServer, ValidSerialNumbers.getAll()[0]);
+
+			Test Test1 = new Test(dSetting1);
+
+			List<Test> tests = new List<Test>();
+			tests.Add(Test1);
+
+			await Program.buildTests(tests);
+
+			foreach (Test nextTest in Program.getTests())
+			{
+				Assert.AreEqual(nextTest.getExpectedResult(), nextTest.getActualResult());
+			}
+
 
         }
 
-        [TestMethod]
+        //[TestMethod]
         public async Task DeviceStatusTest()
         {
+			DeviceStatusJSON testJson1 = new DeviceStatusJSON ();
+			testJson1.intSerial = ValidSerialNumbers.getAll()[1];
+			testJson1.seqNum = "4";
+			testJson1.capture = "1";
+			testJson1.captureMode = "1";
+			testJson1.callHomeTimeoutMode = "0";
+			testJson1.callHomeTimeoutData = null;
+			testJson1.dynCodeFormat = new string[2];
+			testJson1.dynCodeFormat[0] = "[\"~*[1,12]/*[1,63]\"";
+			testJson1.dynCodeFormat[1] = "\"~server/redemption\"]";
+			testJson1.errorLog = new string[0];
+			testJson1.startURL = "http://cozumotesttls.cloudapp.net:80/api/DeviceSetting";
+			testJson1.reportURL = "http://cozumotesttls.cloudapp.net:80/api/DeviceStatus";
+			testJson1.scanURL = "http://cozumotesttls.cloudapp.net:80/api/DeviceScan";
+			testJson1.bkupURL = "http://cozumotesttls.cloudapp.net:80/api/DeviceBackup";
+			testJson1.requestTimeoutValue = "8000";
+			testJson1.cmdChkInt = "1";
+			testJson1.cmdURL = "http://www.cozumo.com:80/callhome";
+			testJson1.revId = "ITCMM15-SBTC-0515-E001";
+			DeviceStatus testDSetting = new DeviceStatus(testServer, testJson1);
+			Test Test1 = new Test(testDSetting);
+
+			List<Test> tests = new List<Test>();
+			tests.Add(Test1);
+
+			await Program.buildTests(tests);
+
+			foreach (Test nextTest in Program.getTests())
+			{
+				Assert.AreEqual(nextTest.getExpectedResult(), nextTest.getActualResult());
+			} 
 
         }
     }
